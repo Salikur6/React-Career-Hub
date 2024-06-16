50ac1df0f9044a7b4fc91137b917c285b35e5656
Salikur Islam <salikur6@gmail.com>
2024-06-16 20:03:36 +0600

    Job Details page Designed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,35 +1,37 @@
 const Footer = () => {
     return (
-        <footer className="footer p-10 text-base-content bg-[#1a1919]">
-            <aside>
+        <div className="bg-[#1a1919]">
+            <footer className="footer container mx-auto p-10 text-base-content">
+                <aside>
 
-                <p className="text-3xl font-bold text-white">CareerHub</p>
+                    <p className="text-3xl font-bold text-white">CareerHub</p>
 
-                <p className="text-[#ffffffb3] w-2/3">
-                    There are many variations of passages of Lorem Ipsum , but the majority have suffered alteration in some form.
-                </p>
-            </aside>
-            <nav className="text-[#ffffffb3]">
-                <h6 className="footer-title text-white opacity-100">Services</h6>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
-            </nav>
-            <nav className="text-[#ffffffb3]">
-                <h6 className="footer-title text-white opacity-100">Company</h6>
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
-            </nav>
-            <nav className="text-[#ffffffb3]">
-                <h6 className="footer-title text-white opacity-100">Legal</h6>
-                <a className="link link-hover" > Terms of use</a>
-                <a className="link link-hover">Privacy policy</a>
-                <a className="link link-hover">Cookie policy</a>
-            </nav>
-        </footer>
+                    <p className="text-[#ffffffb3] w-2/3">
+                        There are many variations of passages of Lorem Ipsum , but the majority have suffered alteration in some form.
+                    </p>
+                </aside>
+                <nav className="text-[#ffffffb3]">
+                    <h6 className="footer-title text-white opacity-100">Services</h6>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </nav>
+                <nav className="text-[#ffffffb3]">
+                    <h6 className="footer-title text-white opacity-100">Company</h6>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </nav>
+                <nav className="text-[#ffffffb3]">
+                    <h6 className="footer-title text-white opacity-100">Legal</h6>
+                    <a className="link link-hover" > Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </nav>
+            </footer>
+        </div>
     )
 }
 
--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,5 +1,7 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import DetailsTopSection from "../DetailsTopSection/DetailsTopSection";
+import { RiMoneyDollarCircleLine } from "react-icons/ri";
+import { MdOutlineSubtitles } from "react-icons/md";
 
 const JobDetails = () => {
     const jobData = useLoaderData();
@@ -8,10 +10,65 @@ const JobDetails = () => {
     const jobIdData = jobData.find(job => job.id === parseInt(id));
 
     console.log(jobIdData)
-    return (
-        <div>
-            <DetailsTopSection title={'Job Details'}></DetailsTopSection>
+    const { job_description, job_responsibility, educational_requirements, experiences, salary, job_title, contact_information } = jobIdData;
 
+    const { phone, email, address } = contact_information;
+
+    console.log(contact_information)
+    return (
+        <div className="mb-24">
+            <DetailsTopSection title={'Job Details'}></DetailsTopSection>
+            <div className="container mx-auto grid-cols-1 grid gap-4 md:grid-cols-5 lg:grid-cols-6">
+
+                <div className="col-span-1 md:col-span-3 lg:col-span-4">
+                    <h3 className="font-bold text-[#1A1919] mb-6">Job Description: <span className="font-semibold text-[#757575]">{job_description}</span></h3>
+                    <h3 className="font-bold text-[#1A1919] mb-6">Job Responsibility: <span className="font-semibold text-[#757575]">{job_responsibility}</span></h3>
+
+                    <div className=" mb-6">
+                        <h3 className="font-bold text-[#1A1919] mb-4">Education Requirements: </h3>
+                        <p>
+                            <span className="font-semibold text-[#757575]">{educational_requirements}</span>
+                        </p>
+                    </div>
+
+                    <div className=" mb-6">
+                        <h3 className="font-bold text-[#1A1919] mb-4">Experiences: </h3>
+                        <p>
+                            <span className="font-semibold text-[#757575]">{experiences}</span>
+                        </p></div>
+                </div>
+
+                <div className="lg:col-span-2 md:col-span-2">
+                    <div className="bg-gradient-to-r from-[#7e90fe1a] to-[#9873ff1a]  p-7 mb-6">
+                        <div className="mb-8">
+                            <h4 className="text-xl font-bold mb-6">Job Details</h4>
+                            <hr className="w-6/7 mx-auto mb-6" />
+
+                            <p className="flex items-center text-lg lg:text-xl font-bold text-[#474747] mb-4"> <RiMoneyDollarCircleLine className="text-2xl mr-2 text-[#9873FF]" /> Salary : <span className="text-[#757575] font-semibold">{salary}</span></p>
+
+                            <p className="flex items-center text-lg lg:text-xl font-bold text-[#474747]">
+                                <MdOutlineSubtitles className="text-2xl mr-2 text-[#9873FF]" />
+                                Job Title :  <span className="text-[#757575] font-semibold"> {job_title}</span>
+                            </p>
+                        </div>
+
+                        <div className="text-xl font-bold mb-6">
+
+                            <h4 className="text-xl font-bold mb-6">Contact Information</h4>
+                            <hr className="w-6/7 mx-auto mb-6" />
+                            <p className="mb-4 text-lg lg:text-xl font-bold text-[#474747]">Phone : <span className="text-[#757575] font-semibold">{phone}</span></p>
+
+                            <p className="mb-4 text-lg lg:text-xl font-bold text-[#474747]">Email : <span className="text-[#757575] font-semibold">{email}</span></p>
+
+                            <p className="mb-4 text-lg lg:text-xl font-bold text-[#474747]">Address : <span className="text-[#757575] font-semibold">{address}</span></p>
+                        </div>
+                    </div>
+                    <div>
+                        <button className={'w-full text-xl font-bold text-white btn bg-gradient-to-r from-[#7E90FE] to-[#9873FF]'}>Apply Now</button>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
